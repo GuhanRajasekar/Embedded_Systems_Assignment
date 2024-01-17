c7ecdbda4c1b9e5a44e54d6f517d275acf275a56
GuhanRajasekar <149869855+GuhanRajasekar@users.noreply.github.com>
2024-01-17 19:47:47 +0530

    Code for Blinky Assignment for Embedded
    This code contains solution up to the part where the LED shifts through all the colors when SW1 (PF4) is pressed.

---
+++ Assignment1_Blinky/blinky_code.c
@@ -0,0 +1,55 @@
+#include <stdint.h>
+#include "inc/tm4c123gh6pm.h"
+// Green, Blue, Cyan, Red, Yellow, Magenta, White
+#define COLOR_GREEN_ON     0x08
+#define COLOR_BLUE_ON      0x04
+#define COLOR_RED_ON       0x02
+#define COLOR_CYAN_ON      0x0C  // B + G
+#define COLOR_YELLOW_ON    0x0A  // R + G
+#define COLOR_MAGENTA_ON   0x06  // B + R
+#define COLOR_WHITE_ON     0x0E  // B + R + G
+#define NO_COLOR           0x00  // No color
+
+void delayMs(int n);
+int main(void)
+{
+SYSCTL_RCGC2_R |= 0x00000020;   /* enable clock to GPIOF at clock gating control register */
+GPIO_PORTF_DIR_R = 0x0E;    /* enable the GPIO pins for the LED (PF3, 2 1) as output */
+GPIO_PORTF_DEN_R = 0x1F;    // Enabling all the 5 pins of port 5 for digital activity
+GPIO_PORTF_PUR_R = 0x11; /* enable pull up for pin 4 (SW1) and pin 0 (SW2). If this is not present, I think SW1 and SW2 are always considered to be pressed*/
+int flag = 0;           // to indicate if reset was pressed
+while(1)
+{
+    int x = GPIO_PORTF_DATA_R & 0x10 ; // keep reading data from PF4 ( SW1 )
+    if(x==0 && flag!=0)
+    {
+        for(;;)   // cycle through the colors forever if SW1 is pressed
+        {
+          GPIO_PORTF_DATA_R = COLOR_GREEN_ON;
+          delayMs(500);
+          GPIO_PORTF_DATA_R = COLOR_BLUE_ON;
+          delayMs(500);
+          GPIO_PORTF_DATA_R = COLOR_RED_ON;
+          delayMs(500);
+          GPIO_PORTF_DATA_R = COLOR_CYAN_ON;
+          delayMs(500);
+          GPIO_PORTF_DATA_R = COLOR_YELLOW_ON;
+          delayMs(500);
+          GPIO_PORTF_DATA_R = COLOR_MAGENTA_ON;
+          delayMs(500);
+          GPIO_PORTF_DATA_R = COLOR_WHITE_ON;
+          delayMs(500);
+          GPIO_PORTF_DATA_R = NO_COLOR;
+        }
+    }
+    flag = 1; // to make sure that the lights are not activated the first time when the reset button is pressed
+
+}
+}
+/* delay n milliseconds (16 MHz CPU clock) */
+void delayMs(int n)
+{
+int i, j;
+for(i = 0 ; i < n; i++)
+for(j = 0; j < 3180; j++) {}    /* do nothing for 1 ms */
+}
