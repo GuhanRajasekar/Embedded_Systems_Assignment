506db68af6d373de6bac032c9912dcae29b54394
GuhanRajasekar <149869855+GuhanRajasekar@users.noreply.github.com>
2024-01-28 19:44:26 +0530

    Crude Working Copy
    This Code is not yet optimized.
    
    The code does the following:
    1). Once we give "Color <Color_Name>" it will start blinking in that color with on delay of 1000ms and  off delay of 1000ms.
    
    2). If any other character is pressed, blinking is stopped
    
    What the code must do.
    1). In the middle of blinking, if we press any key blinking must not stop. And it should only check for blink command or another color command

--- Assignment3_Blinky_Console/uart_blinky.c
+++ Assignment3_Blinky_Console/uart_blinky.c
@@ -1,5 +1,6 @@
 #include <stdint.h>
 #include <stdbool.h>
+#include <string.h>
 #include <./inc/tm4c123gh6pm.h>
 #include <inc/hw_memmap.h>
 #include <inc/hw_types.h>
@@ -8,13 +9,45 @@
 #include <driverlib/sysctl.h>
 #include <driverlib/uart.h>
 
-char console_cmd_buffer[30];  // global character array to store the echo the contents given by the user
-char* Color;                  // global string to store the Color
-int Blink_Rate;               // global integer to store the blink rate
+// Macros for the various colors
+#define COLOR_GREEN_ON     0x08
+#define COLOR_BLUE_ON      0x04
+#define COLOR_RED_ON       0x02
+#define COLOR_CYAN_ON      0x0C  // B + G
+#define COLOR_YELLOW_ON    0x0A  // R + G
+#define COLOR_MAGENTA_ON   0x06  // B + R
+#define COLOR_WHITE_ON     0x0E  // B + R + G
+#define NO_COLOR           0x00  // No color
 
+void delayMs(int n);          // Software function to implement delay
+
+char console_cmd_buffer[30];  // global character array to store the echo the contents given by the user
+char val;                     // global character variable that is used across functions to read data from the console
+int Color = -1;               // global variable to denote which color is currently active
+int Blink_Rate;               // global integer to store the blink rate
+int on_off_delay = 1000;      // Global Variable to denote the rate at which blinking needs to take place
+
+int findColor()               // Function to determine which color needs to be lit
+{
+   if(strstr(console_cmd_buffer ,"Color Green"))   return 1; //Green Color
+   if(strstr(console_cmd_buffer ,"Color Blue"))    return 2; //Blue Color
+   if(strstr(console_cmd_buffer ,"Color Cyan"))    return 3; //Cyan Color
+   if(strstr(console_cmd_buffer ,"Color Red"))     return 4; //Red Color
+   if(strstr(console_cmd_buffer ,"Color Yellow"))  return 5; //Yellow Color
+   if(strstr(console_cmd_buffer ,"Color Magenta")) return 6; //Magenta Color
+   if(strstr(console_cmd_buffer ,"Color White"))   return 7; //White Color
+   return -1;                            //Invalid Data Entered
+}
 int main()
 {
-    char val;
+    // Necessary definitions to write data into Port F
+    SYSCTL_RCGC2_R |=   0x00000020;   /* enable clock to GPIOF at clock gating control register */
+    GPIO_PORTF_DIR_R =  0x0E;        /* enable the GPIO pins for the LED (PF3, 2 1) as output */
+    GPIO_PORTF_LOCK_R = 0x4C4F434B;   /* unlock commit register GPIOCR */
+    GPIO_PORTF_CR_R =   0x01;         /* make PORTF0 configurable */
+    GPIO_PORTF_DEN_R =  0x1F;        // Enabling all the 5 pins of port 5 for digital activity
+    GPIO_PORTF_PUR_R =  0x11;        /* enable pull up for pin 4 (SW1) and pin 0 (SW2). If this is not present, I think SW1 and SW2 are always considered to be pressed*///
+
     int i = 0; // variable used for indexing purposes
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
@@ -33,16 +66,66 @@ int main()
 
             // Check for Enter Character
             // If the received character is 0x0D => Enter key has been pressed
-           if( (val == 0x0D))
+           if((val == 0x0D))
            {
-                 // Enter key has been pressed
-                // process the data here to take necessary action based on the contents of the console_cmd_buffer array
-                // Based on the contents of the array, either change the color or change the blink speed
+               // Enter key has been pressed. Process the contents of the data entered
+               while(!(UARTCharsAvail(UART0_BASE)))  // Keep repeating the following operating until no other key has been pressed
+                {
+                   if(strstr(console_cmd_buffer , "Color"))
+                   {
+                        Color = findColor();  // Find out which color is requested
+                        switch(Color)
+                        {
+                            case 1: GPIO_PORTF_DATA_R = COLOR_GREEN_ON;
+                                    delayMs(on_off_delay);
+                                    GPIO_PORTF_DATA_R = NO_COLOR;
+                                    delayMs(on_off_delay);
+                                    break;
+
+                            case 2: GPIO_PORTF_DATA_R = COLOR_BLUE_ON;
+                                    delayMs(on_off_delay);
+                                    GPIO_PORTF_DATA_R = NO_COLOR;
+                                    delayMs(on_off_delay);
+                                    break;
+
+                            case 3: GPIO_PORTF_DATA_R = COLOR_CYAN_ON;
+                                    delayMs(on_off_delay);
+                                    GPIO_PORTF_DATA_R = NO_COLOR;
+                                    delayMs(on_off_delay);
+                                    break;
+
+                            case 4: GPIO_PORTF_DATA_R = COLOR_RED_ON;
+                                    delayMs(on_off_delay);
+                                    GPIO_PORTF_DATA_R = NO_COLOR;
+                                    delayMs(on_off_delay);
+                                    break;
+
+                            case 5: GPIO_PORTF_DATA_R = COLOR_YELLOW_ON;
+                                    delayMs(on_off_delay);
+                                    GPIO_PORTF_DATA_R = NO_COLOR;
+                                    delayMs(on_off_delay);
+                                    break;
+
+                            case 6: GPIO_PORTF_DATA_R = COLOR_MAGENTA_ON;
+                                    delayMs(on_off_delay);
+                                    GPIO_PORTF_DATA_R = NO_COLOR;
+                                    delayMs(on_off_delay);
+                                    break;
+
+                            case 7: GPIO_PORTF_DATA_R = COLOR_WHITE_ON;
+                                    delayMs(on_off_delay);
+                                    GPIO_PORTF_DATA_R = NO_COLOR;
+                                    delayMs(on_off_delay);
+                                    break;
+
+                            default: GPIO_PORTF_DATA_R = NO_COLOR;   // for debugging purposes
+                                     break;
+                        }
+                   }
+                }
            }
-
-
             // Support Back Space functionality
-            else if(val == 0x08 && i>0)
+           else if(val == 0x08 && i>0)
               {
                   UARTCharPut(UART0_BASE, '\b');   //Move the cursor back by one position to the left
                   UARTCharPut(UART0_BASE, ' ');    // Place a ' ' at the place where the character needs to be removed.
@@ -59,11 +142,27 @@ int main()
                 if(i<=29) console_cmd_buffer[i] = val;
                 UARTCharPut(UART0_BASE, console_cmd_buffer[i]);
                 i = i+1;
-                if(i>=0 && i<=29)  console_cmd_buffer[i] = '\0'; // Terminate the buffer (character array) at a valid position
+                if(i>=0 && i<=29)  console_cmd_buffer[i] = '\0'; // Terminate the array at a valid position
             }
 
 
     }
 
-    return 0;
+  return 0;
+}
+
+/* delay n milliseconds (16 MHz CPU clock) */
+void delayMs(int n)
+{
+   int i, j;
+   for(i = 0 ; i < n; i++)
+     {
+        for(j = 0; j < 3180; j++)
+            {
+                /* do nothing for 1 ms */
+                // check for key press periodically
+            }
+      }
+
+   return;  // sw1 not pressed during the entire duration of the delay
 }
