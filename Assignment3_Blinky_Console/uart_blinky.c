82ba274d08b425db5acdb48f6c3d075abfb31619
GuhanRajasekar <149869855+GuhanRajasekar@users.noreply.github.com>
2024-01-28 22:49:58 +0530

    Majaa code
    This is Majaa code bhaaa..
    
    This code can do the following sambavams:
    
    1). Retain the previous state of LED blink while user enters new command
    
    2). This feature also works for even lower delays as we are now monitoring the user input for every 1ms.
    
    To do:
    1). Give a prompt to user if invalid command has been given by user
    
    2). Right now blink delay has been implemented for blinking of 1,2,4,8,16,32 times in 2 seconds. Change the blink number to "ARBITRARY NUMBER OF TIMES A MINUTE ".

--- Assignment3_Blinky_Console/uart_blinky.c
+++ Assignment3_Blinky_Console/uart_blinky.c
@@ -23,8 +23,9 @@ void delayMs(int n);          // Software function to implement delay
 
 char console_cmd_buffer[30];  // global character array to store the echo the contents given by the user
 char val;                     // global character variable that is used across functions to read data from the console
+int id = 0;                // global variable to take care of indexing of the character buffer array
 int Color = -1;               // global variable to denote which color is currently active
-int Blink_Delay = 61.25;        // global integer to store the blink rate
+int Blink_Delay = 1000;        // global integer to store the blink rate
 
 int findColor()               // Function to determine which color needs to be lit
 {
@@ -41,12 +42,12 @@ int findColor()               // Function to determine which color needs to be l
 int findBlinkRate()
 {
     if     (strstr(console_cmd_buffer ,"Blink 16"))  return 61.25;
-    if(strstr(console_cmd_buffer ,"Blink 2"))        return 61.25; //500;
-    if(strstr(console_cmd_buffer ,"Blink 4"))        return 61.25; //250;
-    if(strstr(console_cmd_buffer ,"Blink 8"))        return 61.25; //125;
-    if(strstr(console_cmd_buffer ,"Blink 1"))        return 61.25; //1000;  // Not happening as Blink 1 is a subset of Blink 16. Hence Blink_Rate is being set as 1000
-    if(strstr(console_cmd_buffer ,"Blink 32"))       return 61.25; //31.25;
-    return 61.25;//1000; // Default value
+    if(strstr(console_cmd_buffer ,"Blink 2"))        return 500;
+    if(strstr(console_cmd_buffer ,"Blink 4"))        return 250;
+    if(strstr(console_cmd_buffer ,"Blink 8"))        return 125;
+    if(strstr(console_cmd_buffer ,"Blink 1"))        return 1000;// Placing this here otherwise Blink 16 is not being detected as "Blink 1" is a substring of "Blink 16"
+    if(strstr(console_cmd_buffer ,"Blink 32"))       return 31.25;
+    return 1000;//1000; // Default value
 }
 void processColors()
 {
@@ -101,6 +102,37 @@ void processColors()
     return;
 }
 
+// Function to handle Enter Key presses
+void processEnterKey()
+{
+    // Enter key has been pressed. Process the contents of the data entered
+    if(strstr(console_cmd_buffer , "Color") || strstr(console_cmd_buffer , "Blink"))
+    {
+         if(strstr(console_cmd_buffer , "Color"))        Color      = findColor();                // Find out which color is requested
+         else if(strstr(console_cmd_buffer , "Blink"))   Blink_Delay = findBlinkRate();            // Find out the blink rate
+    }
+}
+
+// Function to handle BackSpace Key presses
+void processBackSpaceKey()
+{
+    UARTCharPut(UART0_BASE, '\b');   //Move the cursor back by one position to the left
+    UARTCharPut(UART0_BASE, ' ');    // Place a ' ' at the place where the character needs to be removed.
+                                     // Once ' ' is written, cursor will once again move by one position to the right. We need to move it back again.
+    UARTCharPut(UART0_BASE, '\b');   // Move the cursor back again.
+    id = id-1;                         // Decrement the index as a character has been removed
+    console_cmd_buffer[id] = '\0';    // Terminate the character array at the appropriate position
+}
+
+// Function to handle key press that is neither "Enter" nor "Back Space"
+void processNormalKey()
+{
+    //UARTCharPut(UART0_BASE, val);  // Echo from the TIVA board back to the PC
+    if(id<=29) console_cmd_buffer[id] = val;
+    UARTCharPut(UART0_BASE, console_cmd_buffer[id]);
+    id = id +1;
+    if(id>=0 && id<=29)  console_cmd_buffer[id] = '\0'; // Terminate the array at a valid position
+}
 int main()
 {
     // Necessary definitions to write data into Port F
@@ -112,7 +144,6 @@ int main()
     GPIO_PORTF_DEN_R =  0x1F;        // Enabling all the 5 pins of port 5 for digital activity
     GPIO_PORTF_PUR_R =  0x11;        /* enable pull up for pin 4 (SW1) and pin 0 (SW2). If this is not present, I think SW1 and SW2 are always considered to be pressed*///
 
-    int index = 0;                    // Variable used for indexing of the global character buffer array
     SysCtlPeripheralEnable(SYSCTL_PERIPH_UART0);
     SysCtlPeripheralEnable(SYSCTL_PERIPH_GPIOA);
 
@@ -125,44 +156,13 @@ int main()
     {
         while(!(UARTCharsAvail(UART0_BASE)))
           {
-            processColors();  // Retain the previous state when there is no new command
+            processColors();  // Maintain LED Blink state even when there is no new command
           }
-            val = UARTCharGet(UART0_BASE);
-
-            // Check for Enter Character
-            // If the received character is 0x0D => Enter key has been pressed
-           if((val == 0x0D))
-           {
-                   // Enter key has been pressed. Process the contents of the data entered
-                   if(strstr(console_cmd_buffer , "Color") || strstr(console_cmd_buffer , "Blink"))
-                   {
-                        if(strstr(console_cmd_buffer , "Color"))        Color      = findColor();                // Find out which color is requested
-                        else if(strstr(console_cmd_buffer , "Blink"))   Blink_Delay = findBlinkRate();            // Find out the blink rate
-                   }
-
-           }
-            // Support Back Space functionality
-           else if(val == 0x08 && index>0)
-              {
-                  UARTCharPut(UART0_BASE, '\b');   //Move the cursor back by one position to the left
-                  UARTCharPut(UART0_BASE, ' ');    // Place a ' ' at the place where the character needs to be removed.
-                                                   // Once ' ' is written, cursor will once again move by one position to the right. We need to move it back again.
-                  UARTCharPut(UART0_BASE, '\b');   // Move the cursor back again.
-                  index = index-1;                         // Decrement the index as a character has been removed
-                  console_cmd_buffer[index] = '\0';    // Terminate the character array at the appropriate position
-
-              }
-
-            // Process normal characters other than 'Enter' and 'BackSpace'
-            else
-            {
-                //UARTCharPut(UART0_BASE, val);  // Echo from the TIVA board back to the PC
-                if(index<=29) console_cmd_buffer[index] = val;
-                UARTCharPut(UART0_BASE, console_cmd_buffer[index]);
-                index = index +1;
-                if(index>=0 && index<=29)  console_cmd_buffer[index] = '\0'; // Terminate the array at a valid position
-            }
-           processColors();
+        val = UARTCharGet(UART0_BASE);
+        if((val == 0x0D))                 processEnterKey();         // If entered character is 0x0D => Enter key has been pressed
+        else if(val == 0x08 && id>0)      processBackSpaceKey();     // If entered character is 0x08 => Back Space Key has been pressed
+        else                              processNormalKey();        // To process Key press that is Neither "Enter" nor "Back Space"
+        processColors();                                             // Maintain LED blink state even in the middle of a new command
     }
 
   return 0;
@@ -179,7 +179,15 @@ void delayMs(int n)
                 /* do nothing for 1 ms */
                 // check for key press periodically
             }
+        // After every 1ms check the input entered by the user
 
+       if(UARTCharsAvail(UART0_BASE))
+       {
+           val = UARTCharGet(UART0_BASE);
+           if((val == 0x0D))                 processEnterKey();         // If entered character is 0x0D => Enter key has been pressed
+           else if(val == 0x08 && id>0)      processBackSpaceKey();     // If entered character is 0x08 => Back Space Key has been pressed
+           else                              processNormalKey();        // To process Key press that is Neither "Enter" nor "Back Space"
+       }
       }
    return;  // sw1 not pressed during the entire duration of the delay
 }
